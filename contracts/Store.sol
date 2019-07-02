ffb2df3f91c874f257e7d7989fc357583910226d
phavaha1 <phamvanhau123321@gmail.com>
2019-07-02 14:28:16 +0700

    event in solidity

--- contracts/Store.sol
+++ contracts/Store.sol
@@ -1,10 +1,17 @@
 pragma solidity ^0.5.8;
 
+
 contract SimpleStorage {
   uint myVariable;
-
+  event Odd();
+  event Even();
   function set(uint x) public {
     myVariable = 2*x;
+    if(x % 2 == 0){
+      emit Odd();
+    }else{
+      emit Even();
+    }
   }
 
   function get() public view returns (uint) {
